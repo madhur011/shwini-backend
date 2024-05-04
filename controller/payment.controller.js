fd3e53f615934b7be3b905917f6fab2f7e4ea9a2
multikodit011 <multikodit@gmail.com>
2024-05-04 13:19:54 +0530

    ..

--- controller/payment.controller.js
+++ controller/payment.controller.js
@@ -59,7 +59,7 @@ exports.paymentVerification = async (req, res) => {
   try {
     const settingKeys = await getSetting("getMe");
 
-    const hmac = crypto.createHmac("sha256", settingKeys.razorKey);
+    const hmac = crypto.createHmac("sha256", settingKeys.razorApiSecret);
     const data = `${razorpay_order_id}|${razorpay_payment_id}`;
     hmac.update(data);
     const generated_signature = hmac.digest("hex");
